4636cadba827a25ba440acc02a5d88c99611c151
Rafael Barbosa <lul.rafaelbarbosa@gmail.com>
2025-03-15 15:20:35 -0300

    feat: initial calibration

--- src/calibrate_probabilities.py
+++ src/calibrate_probabilities.py
@@ -4,7 +4,8 @@ from utils_clean_data import load_and_clean_data
 from utils_modelling import *
 import yaml
 from pathlib import Path
-from sklearn.calibration import calibration_curve
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
+from sklearn.metrics import brier_score_loss
 import pandas as pd
 import mlflow
 
@@ -23,6 +24,7 @@ FEATURES = config['modelling']['features']
 TARGET = config['modelling']['target']
 METRIC_NAME = config['modelling']['metric_name']
 MODEL_PATH = config['modelling']['model_path']
+CALIBRATED_PATH = config['modelling']['calibrated_model_path']
 
 # Load data
 
@@ -31,11 +33,45 @@ y_test = pd.read_csv(PROCESSED_DATA_PATH / 'y_test.csv')
 
 # Load model 
 
-model_dirs = [d for d in Path(MODEL_PATH).iterdir() if d.is_dir() and d.name.startswith("best_model_")]
+id_model = '7ab7bfd0c8d54b87a3ce7d6eb2ede953'
+best_model_path = f'{MODEL_PATH}/best_model_{id_model}/'
 
-# model = mlflow.sklearn.load_model()
-print(model_dirs)
+best_model = mlflow.sklearn.load_model(best_model_path)
 
-# Load pipeline 
+# Aplicar o pré-processamento aos dados de validação
 
+X_test_transformed = best_model.named_steps['preprocessor'].transform(X_test)
+print("Pré-processamento aplicado aos dados de validação.")
+print(X_test_transformed)
 
+# Calibrar o modelo
+
+best_model_class = best_model.named_steps['classifier']
+
+calibrated_model = CalibratedClassifierCV(best_model_class, method='sigmoid', cv='prefit')
+calibrated_model.fit(X_test_transformed, y_test)
+
+# Prever probabilidades calibradas
+
+y_pred_proba_calibrated = calibrated_model.predict_proba(X_test_transformed)[:, 1]
+
+# Avaliar a calibragem com a Curva de Confiabilidade
+fraction_of_positives, mean_predicted_value = calibration_curve(y_test, y_pred_proba_calibrated, n_bins=10)
+
+# Plotar a Curva de Confiabilidade
+plt.plot(mean_predicted_value, fraction_of_positives, "s-", label="Calibrado")
+plt.plot([0, 1], [0, 1], "k:", label="Perfeitamente Calibrado")
+plt.ylabel("Fração de Positivos")
+plt.xlabel("Probabilidade Média Prevista")
+plt.legend()
+plt.title("Curva de Confiabilidade")
+plt.savefig(f"confusion_matrix_calibrated.png")
+plt.close()
+print("Curva de confiabilidade salva como 'confusion_matrix_calibrated.png'.")
+
+# Calcular o Brier Score
+brier_score = brier_score_loss(y_test, y_pred_proba_calibrated)
+print(f"Brier Score após calibração: {brier_score}")
+
+# Salvar o modelo calibrado em uma nova pasta 
+# Melhorar o nome da pasta 
